8e8b4e56bed18890b1d6e57f0c3e236571ee7056
Matheus Rangel De Melo <matheus_rangel@A309-15L.imd.local>
2019-09-05 18:31:54 -0300

    Primeiro commit

---
+++ server.py
@@ -0,0 +1,10 @@
+import socketserver
+KEY = ""
+class Client():
+    def __init__(self, info, ):
+        
+class ChatHandler():
+
+if __name__ == "__main__":
+    host, port = "0.0.0.0", 42422
+    with socketserver.ThreadingTCPServer((host, port), )
